61cd917235968c5f71d6018ccba05248489780f3
Ashish Kumar <ashishkumar141199@gmail.com>
2026-04-22 17:23:08 +0530

    chack if dev branch is working or not

--- app/Controller/ProductsController.php
+++ app/Controller/ProductsController.php
@@ -46,7 +46,7 @@ class ProductsController extends AppController
 
 
 
-    public function add()
+     public function add()
     {
         if ($this->request->is('post')) {
             $this->Product->save($this->request->data);
